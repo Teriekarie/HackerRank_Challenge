839fcbffba5d4d83078a70c8ca988f6e826dfe4b
Theresa Karamor <teriekarie@gmail.com>
2022-09-26 20:36:20 +0100

    first commit

---
+++ Day_0/Hello_world.py
@@ -0,0 +1,10 @@
+# Python program to read a full line of input from stdin 
+# and save it to our dynamically typed variable, input_string.
+
+input_string = input() #allow system to input 'welcome to 30days of coding'
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# Write a line of code here that prints the contents of input_string to stdout.
+print(str(input_string))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HackerRank_Challenge
+
+This is a 3odays coding challenge i started to improve my coing skills.
+So ride along with me on this journey.
